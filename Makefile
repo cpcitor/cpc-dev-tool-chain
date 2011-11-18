a85f575c0213cf051903a0be971af00139e08bd5
cpcitor <gouri@free.fr>
2011-11-18 23:28:01 +0100

    Makefile+patches to build z88dk, iDSK and a hello world for Amstrad CPC.
    This is an embryo. From your local copy, a simple "make" should fetch
    source code and build :
    
    * z88dk toolchain including zcc emulator and create a build_config.inc
      to source for easy use
    
    * iDSK tool to manipulate Amstrad CPC 3" disk image file.
    
    * a hello world using both tools above.
    
    You should be able to run the hello world like this :
    
    * launch an emulator, telling it to put a.dsk in first disk drive
    
    * inside emulated CPC type :
    
    memory &5fff
    load "a.bin
    call &6000
    
    CPC should show :
    
    Hello world !

--- Makefile
+++ Makefile
@@ -4,7 +4,12 @@
 # others : projects
 
 # Makefile in project knows its dependencies.
-all: findyway/findyway.dsk
+#all: findyway/findyway.dsk
 
 
 
+all: hello_world_using_z88dk/Makefile
+	LC_ALL=C make -C hello_world_using_z88dk
+
+#distclean:
+#	( find . -type d -print -exec bash -c "cd '{}' ; make clean ; make mrproper ; make distclean" \; ; )
---
+++ hello_world_using_z88dk/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int
+fillscreen (unsigned char a)
+{
+  unsigned char *p = 0xc000;
+  unsigned short l;
+
+  for (l = 0; l < 16384; l++) {
+    *p = a;
+    p++;
+  }
+}
+
+int
+main (int argc, char **argv)
+{
+  printf ("Hello world!\n");
+  {
+    char c;
+
+    for (c = 0; c <= 255; c++) {
+      fillscreen (c);
+    }
+  }
+  /* for (a=32; a<256; a++) { putc(a, stdout); } */
+  return 0;
+}

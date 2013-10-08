1d2ccc7210e816baa53866335b48ba0ef39ce559
cpcitor <gouri@free.fr>
2013-10-08 11:43:47 +0200

    Script to automatically download, compile and make available: sdcc.

---
+++ tool/sdcc/sdcc_configure.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This script is intended to be called by make.
+# Usage: scriptname buildtree installtree sourcetree
+
+set -eu
+
+set -xv
+
+BUILDTREE="$1"
+INSTALLTREE="$2"
+SOURCETREE="$3"
+
+cd "$SOURCETREE"
+SOURCETREE_ABS="$PWD"
+cd -
+
+mkdir -p "${INSTALLTREE}"
+cd "${INSTALLTREE}"
+INSTALLTREE_ABS="$PWD"
+cd -
+
+mkdir -p "${BUILDTREE}"
+cd "${BUILDTREE}"
+BUILDTREE_ABS="$PWD"
+
+$SOURCETREE_ABS/configure --prefix="${INSTALLTREE_ABS}" \
+        --disable-mcs51-port \
+        --disable-z180-port \
+        --disable-r2k-port \
+        --disable-r3ka-port \
+        --disable-gbz80-port \
+        --disable-ds390-port \
+        --disable-ds400-port \
+        --disable-pic14-port \
+        --disable-pic16-port \
+        --disable-hc08-port \
+        --disable-s08-port
+
+# exit code will tell if we succeeded.

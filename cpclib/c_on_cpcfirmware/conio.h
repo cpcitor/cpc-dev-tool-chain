d639777967c196ed5b72c042f223e9c35cbc6729
cpcitor <gouri@free.fr>
2013-10-28 20:17:58 +0100

    Make conion.h work.

--- cpclib/c_on_cpcfirmware/conio.h
+++ cpclib/c_on_cpcfirmware/conio.h
@@ -1,38 +1,35 @@
-/* A Graphics library for the AMSTRAD CPC range of computers
-   Uses firmware to realize graphics output..
-   2003 H. Hansen
-*/
+#ifndef  __CONIO_H__
+#define __CONIO_H__
 
-#ifndef  __conio_h__
-#define __conio_h__
+#include <stdarg.h> // for va_list
 
-; void clrscr (void);
-; unsigned char kbhit (void);
-; void gotox (unsigned char x);
-; void gotoy (unsigned char y);
-; void gotoxy (unsigned char x, unsigned char y)
-; unsigned char wherex (void);
-; unsigned char wherey (void);
-; void cputc (char c);
-; void cputcxy (unsigned char x, unsigned char y, char c)
-; void cputs (const char* s);
-; void cputsxy (unsigned char x, unsigned char y, const char* s);
-; int cprintf (const char* format, ...);
-; int vcprintf (const char* format, va_list ap);
-; char cgetc (void);
-; unsigned char cursor (unsigned char onoff);
-; unsigned char revers (unsigned char onoff);
-; unsigned char textcolor (unsigned char color);
-; unsigned char bgcolor (unsigned char color);
-; unsigned char bordercolor (unsigned char color);
-; void chline (unsigned char length);
-; void chlinexy (unsigned char x, unsigned char y, unsigned char length);
-; void cvline (unsigned char length);
-; void cvlinexy (unsigned char x, unsigned char y, unsigned char length);
-; void cclear (unsigned char length);
-; void cclearxy (unsigned char x, unsigned char y, unsigned char length);
-; void screensize (unsigned char* x, unsigned char* y);
-; void cputhex8 (unsigned char val);
-; void cputhex16 (unsigned val);
+void clrscr (void);
+unsigned char kbhit (void);
+void gotox (unsigned char x);
+void gotoy (unsigned char y);
+void gotoxy (unsigned char x, unsigned char y);
+unsigned char wherex (void);
+unsigned char wherey (void);
+void cputc (char c);
+void cputcxy (unsigned char x, unsigned char y, char c);
+void cputs (const char* s);
+void cputsxy (unsigned char x, unsigned char y, const char* s);
+int cprintf (const char* format, ...);
+int vcprintf (const char* format, va_list ap);
+char cgetc (void);
+unsigned char cursor (unsigned char onoff);
+unsigned char revers (unsigned char onoff);
+unsigned char textcolor (unsigned char color);
+unsigned char bgcolor (unsigned char color);
+unsigned char bordercolor (unsigned char color);
+void chline (unsigned char length);
+void chlinexy (unsigned char x, unsigned char y, unsigned char length);
+void cvline (unsigned char length);
+void cvlinexy (unsigned char x, unsigned char y, unsigned char length);
+void cclear (unsigned char length);
+void cclearxy (unsigned char x, unsigned char y, unsigned char length);
+void screensize (unsigned char* x, unsigned char* y);
+void cputhex8 (unsigned char val);
+void cputhex16 (unsigned val);
 
-#endif /* __conio_h__ */
+#endif /* __CONIO_H__ */
--- tests/confwtest/cfwtest.c
+++ tests/confwtest/cfwtest.c
@@ -1,5 +1,9 @@
 #include <amsgraph.h>
+#include <conio.h>
 
 void main() {
 	draw(320,200);
+
+	cputs("Press any key to exit.");
+	cgetc();
 }

21add4b005a30be85089097d468e2bf714205301
cpcitor <stephane_cpcitor@gourichon.org>
2017-10-31 23:22:12 +0100

    Reorder, comment.

--- cpclib/cfwi/include/cfwi/fw_km.h
+++ cpclib/cfwi/include/cfwi/fw_km.h
@@ -3,13 +3,17 @@
 
 #include <stdbool.h>
 
-unsigned char fw_km_wait_char (void);
-int fw_km_read_char (void);
-unsigned char fw_km_wait_key (void);
-int fw_km_read_key (void);
-
 void fw_km_initialise(void);
 void fw_km_reset(void);
+
+unsigned char fw_km_wait_char (void);
+int fw_km_read_char (void);
+
+unsigned char fw_km_wait_key (void);
+
+/** Contrary to the firmware which sets carry to tell if return value is valid, C wrapper return 0 if no valid value. */
+int fw_km_read_key (void);
+
 void fw_km_disarm_break(void);
 void fw_km_break_event(void);
 void fw_km_flush(void);

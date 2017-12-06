f7970d3889673a8503df04725c56b0e7aa17dd36
cpcitor <stephane_cpcitor@gourichon.org>
2017-12-06 22:52:08 +0100

    Fix comments.

--- cpclib/cfwi/src/fw_txt_set_m_table.s
+++ cpclib/cfwi/src/fw_txt_set_m_table.s
@@ -3,9 +3,9 @@
 _fw_txt_set_m_table::
         ld      hl,#5
         add     hl,sp
-        ld      e,(hl) 		; buffer size
+        ld      e,(hl) 		; lowest affected character
         dec     hl
-        ld      d,(hl)		; is buffer already initialized?
+        ld      d,(hl)		; is buffer already initialized? (can also be considered high byte of 16bit DE since anything 256 or more disables the feature)
 	dec	hl
 	ld	a,(hl)		; buffer address MSB
 	dec	hl

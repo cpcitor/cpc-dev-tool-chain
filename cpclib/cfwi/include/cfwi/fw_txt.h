152d5f33907c91d0c7c5b18565b6811fcac9af2c
cpcitor <stephane_cpcitor@gourichon.org>
2017-12-07 19:24:48 +0100

    Add some documentation.

--- cpclib/cfwi/include/cfwi/fw_txt.h
+++ cpclib/cfwi/include/cfwi/fw_txt.h
@@ -3,10 +3,104 @@
 
 #include <stdint.h>
 
+/** 26: TXT INITIALISE #BB4E
+    Initialise the Text VDU.
+    Action:
+    Full initialization of the Text VDU (as used during EMS). All Text VDU variables and
+    indirections are initialized, the previous VDU state is lost.
+    Entry conditions:
+    No conditions:
+    Exit conditions:
+    AF,BC,DE and HL corrupt. All other registers preserved.
+    Notes:
+    The Text VDU indirections (TXT DRAW CURSOR, TXT UNDRAW CURSOR, TXT
+    WRITE CHAR, TXT UNWRITE and TXT OUT ACTION) are set to their default
+    routines.
+    The control code table is set up to perform the default control code actions.
+    The user defined character table is set to empty.
+    Stream 0 is selected.
+    All streams are set to their default states:
+    The text paper (background) is set to ink 0. The text pen (foreground) is set to ink 1.
+    The text window is set to the entire screen. The text cursor is enabled but turned off.
+    The character write mode is set to opaque.
+    The VDU is enabled. The graphics character write mode is turn off. The cursor is
+    moved to the top left corner of the window.
+    The default character set and the default setting for the control code table are
+    described in Appendices VI and VII.
+    Related entries:
+    SCR INITIALISE
+    TXT RESET
+*/
 void fw_txt_initialise(void);
+
+/** 27: TXT RESET
+    #BB51
+    Reset the Text VDU.
+    Action:
+    Reinitialize the Text VDU indirections and the control code tables. Does not affect
+    any other aspect of the Text VDU.
+    Entry conditions:
+    No conditions.
+    Exit conditions:
+    AF,BC,DE and HL corrupt. All other registers preserved.
+    Notes:
+    The Text VDU indirections TXT DRAW CURSOR, TXT UNDRAW CURSOR, TXT
+    WRITE CHAR, TXT UNWRITE and TXT OUT ACTION are set to their default
+    routines.
+    The control code table is set up to perform the default control code actions (see
+    Appendix VII).
+    Related entries:
+    TXT INITIALISE
+*/
 void fw_txt_reset(void);
+
+/** 28: TXT VDU ENABLE #BB54
+    Allow characters to be placed on the screen.
+    Action:
+    Permit characters to be printed when requested (by calling TXT OUTPUT or TXT WR
+    CHAR). Enabling applies to the currently selected stream. The cursor blob is also
+    enabled (by calling TXT CUR ENABLE).
+    Entry conditions:
+    No conditions.
+    Exit conditions:
+    AF corrupt. All other registers preserved.
+    Notes:
+    The control code buffer used by TXT OUTPUT is emptied, any incomplete control
+    code sequence will be lost.
+    Related entries:
+    TXT ASK STATE
+    TXT CUR ENABLE
+    TXT OUTPUT
+    TXT VDU DISABLE
+    TXT WR CHAR
+*/
 void fw_txt_vdu_enable(void);
+
+/** 29: TXT VDU DISABLE #BB57
+    Prevent character being placed on the screen.
+    Action:
+    Prevents characters being printed on the screen (when TXT OUTPUT or TXT WR
+    CHAR is called). Applies to the currently selected stream. The cursor blob is also
+    disabled (by calling TXT CUR DISABLE).
+    Entry conditions:
+    No conditions.
+    Exit conditions:
+    AF corrupt. All other registers preserved.
+    Notes:
+    The control code buffer used by TXT OUTPUT is emptied, any incomplete control
+    code sequence will be lost.
+    In V1.1 firmware control codes are still obeyed by TXT OUTPUT. In V1.1 firmware
+    only those control codes which are marked in the control code table will be obeyed;
+    other control codes will be ignored (see section 4.7).
+    Related entries:
+    TXT ASK STATE
+    TXT CUR ENABLE
+    TXT OUTPUT
+    TXT VDU DISABLE
+    TXT WR CHAR
+*/
 void fw_txt_vdu_disable(void);
+
 void fw_txt_cur_enable(void);
 void fw_txt_cur_disable(void);
 void fw_txt_cur_on(void);

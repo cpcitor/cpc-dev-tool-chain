9865d22c933827cfb4b37cfb3c6178306da4c46a
Stephane Gourichon (cpcitor) <stephane_cpcitor@gourichon.org>
2017-12-28 19:04:38 +0100

    Add DISTRUST_PRESERVE_REGS compile-time option.

--- cpclib/cfwi/include/cfwi/cfwi.h
+++ cpclib/cfwi/include/cfwi/cfwi.h
@@ -1,6 +1,14 @@
 #ifndef  __CFWI_H__
 #define __CFWI_H__
 
+#ifdef DISTRUST_PRESERVE_REGS
+// This can be useful if you encounter a bad behavior and suspect
+// registers aren's preserved by the firmware conforming to
+// documentation.  Such suspicion happened to me, and this allows to
+// quickly assert that it was not the problem.
+#define __preserves_regs(...) 
+#endif /* DISTRUST_PRESERVE_REGS */
+
 #include "cfwi_txt.h"
 #include "fw_cas.h"
 #include "fw_gra.h"

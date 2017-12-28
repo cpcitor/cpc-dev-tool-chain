e8f9cd88c408ce25b5140b3ec7b3f4977f147655
cpcitor <stephane_cpcitor@gourichon.org>
2017-12-28 05:46:43 +0100

    Add test.

---
+++ cpclib/cfwi/test/gra_plot_absolute/cfwitest.c
@@ -0,0 +1,25 @@
+#include "cfwi/cfwi.h"
+
+void
+main ()
+{
+	{
+		static unsigned char x, y;
+
+		for (x = 0; x < 255; x++)
+		{
+
+			for (y = 0; y < 255; y++)
+			{
+				if ((x ^ y) > x)
+				{
+					fw_gra_plot_absolute(2 * x + 1, 2 * y + 1);
+				}
+			}
+
+		}
+	}
+
+	cfwi_txt_str0_output("Press any key to exit.");
+	fw_km_wait_key();
+}

9227077255ec6f6b5eed9d970971b5696912bf86
Stephane Gourichon (cpcitor) <stephane_cpcitor@gourichon.org>
2018-01-05 16:16:12 +0100

    Add documentation and __preserves_regs to CAS NOISY.

--- cpclib/cfwi/include/cfwi/fw_cas.h
+++ cpclib/cfwi/include/cfwi/fw_cas.h
@@ -1,9 +1,39 @@
 #ifndef  __FW_CAS_H__
 #define __FW_CAS_H__
 
-void fw_cas_initialise(void);
 void fw_cas_in_abandon(void);
 void fw_cas_return(void);
 void fw_cas_out_abandon(void);
+
+/** 119: CAS INITIALISE
+    #BC65
+    Initialize the Cassette Manager.
+    Action:
+    Full initialization of the Cassette Manager (as used during EMS).
+    Entry conditions:
+    No conditions.
+    Exit conditions:
+    AF, BC, DE and HL corrupt.
+    All other registers preserved.
+    Notes:
+    Operations carried out are:
+    All streams are marked closed.
+    The default write speed is set up.
+    The prompt messages are turned off.
+    The cassette motor is turned off (except on V1.0 firmware).
+    Related entries:
+    CAS
+    CAS
+    CAS
+    CAS
+    CAS
+    IN ABANDON
+    NOISY
+    OUT ABANDON
+    SET SPEED
+    STOP MOTOR
+*/
+void fw_cas_initialise(void) __preserves_regs(iyh, iyl);
+
 
 #endif /* __FW_CAS_H__ */

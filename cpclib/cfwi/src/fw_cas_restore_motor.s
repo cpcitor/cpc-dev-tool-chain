6bf9403358c1534d6c14d80d1b25ed24e883ca69
Stéphane Gourichon (cpcitor) <stephane_cpcitor@gourichon.org>
2018-01-17 16:08:30 +0100

    Fix cassette restore motor.

--- cpclib/cfwi/src/fw_cas_restore_motor.s
+++ cpclib/cfwi/src/fw_cas_restore_motor.s
@@ -1,8 +1,8 @@
-.module fw_cas_stop_motor
+.module fw_cas_restore_motor
 
-_fw_cas_stop_motor::
+_fw_cas_restore_motor::
 	ld	a,l
-	call	0xBC74	; CAS STOP MOTOR
+	call	0xBC74	; CAS RESTORE MOTOR
 	xor	a,a
 	ret	nc
 	dec	a

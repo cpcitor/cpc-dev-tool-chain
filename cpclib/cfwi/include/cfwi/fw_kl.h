883813fd63504921f6151d432e0e035aa5fa9b08
Stéphane Gourichon (cpcitor) <stephane_cpcitor@gourichon.org>
2019-05-09 17:46:08 +0200

    Fix wrong prototype.

--- cpclib/cfwi/include/cfwi/fw_kl.h
+++ cpclib/cfwi/include/cfwi/fw_kl.h
@@ -129,7 +129,7 @@ typedef union fw_memory_range_t
     KL INIT BACK
     KL LOG EXT
 */
-uint32_t fw_kl_rom_walk__fastcall(uint32_t fw_memory_range_t_asint) __preserves_regs(iyh, iyl);
+uint32_t fw_kl_rom_walk(uint32_t fw_memory_range_t_asint) __z88dk_fastcall __preserves_regs(iyh, iyl);
 
 // void fw_kl_scan_needed(void);
 

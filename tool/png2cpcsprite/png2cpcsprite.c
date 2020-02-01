c9c36230644b519978836dd52799bdc596e7f0c5
Stéphane Gourichon (cpcitor) <stephane_cpcitor@gourichon.org>
2020-02-01 23:44:24 +0100

    Now using argp.

--- tool/png2cpcsprite/png2cpcsprite.c
+++ tool/png2cpcsprite/png2cpcsprite.c
@@ -5,40 +5,214 @@
 #include <string.h>
 #include <zlib.h>
 
-void show_usage()
+#include <argp.h>
+#include <stdbool.h>
+
+const char *argp_program_version = "png2cpcsprite 0.1";
+const char *argp_program_bug_address = "<stephane_cpcitor@gourichon.org>";
+
+#define symbol_format_string_default "sprite_%s"
+#define module_format_string_default "module_%s"
+
+static char doc[] =
+        "\n"
+        "png2cpcsprite by Stéphane Gourichon (cpcitor).\n"
+        "\n"
+        "Convert a PNG image into a binary representation suitable for "
+        "display by an Amstrad CPC hardware or emulator.\n"
+        "\n"
+        "That binary representation is in turn written in a assembly source "
+        "text file with metadata available as symbols: byte_width, "
+        "pixel_width, height, byte_size, and crtc_mode.\n"
+        "\n"
+        "Notice:\n"
+        "* The input PNG file must be colormap-based.\n"
+        "* The actual palette is ignored by this program.";
+// static char args_doc[] = "[FILENAME]...";
+static struct argp_option options[] = {
+        {0, 0, 0, 0, "Input/output", 1},
+        {"input", 'i', "<input_filename.png>", 0,
+         "Path to an input file in PNG format with a palette (colormap).", 1},
+        {"output", 'o', "<output_filename.s>", 0,
+         "Path where the output file will be written in assembly source "
+         "format.",
+         1},
+        {0, 0, 0, 0, "Output format", 2},
+        {"mode", 'm', "<cpc-mode>", 0,
+         "CPC-mode 0, 1 or 2.  "
+         "If unspecified or '-' the mode will be guessed from the number or "
+         "colormap entries.  Make sure that your image doesn't include extra "
+         "unused palette entries which would confuse the guessing logic.",
+         2},
+        {"direction", 'd', "<t> or <b>", 0,
+         "Default 't' is to write sprite data top to bottom. 'b' causes "
+         "processing bottom to top.  Correct value depend on your context, "
+         "especially sprite write routine.",
+         2},
+        {0, 0, 0, 0, "Assembly-level naming", 3},
+        {"name_stem", 'n', "somename", 0,
+         "String associated with the image.  Think like the name of your image "
+         "at assembly level.  "
+         "Default is to generate a name from the file part in the 'input' "
+         "argument, replacing invalid characters with an underscore '_'.",
+         3},
+        {"symbol_format_string", 1, "<myprefix_%s_mysuffix>", 0,
+         "Format string to generate symbol names.  "
+         "A '%s' is mandatory else the generated assembly file will be invalid."
+         "Default is '" symbol_format_string_default "'.",
+         3},
+        {"module_format_string", 2,
+         "<myprefix_%s_mysuffix> or <my_module_name>", 0,
+         "Format string to generate an assembly module name.  "
+         "It is okay to not include a '%s' so that several generated source "
+         "files belong to the same module.  "
+         "Default is ' " module_format_string_default "'.",
+         3},
+        {0}};
+
+struct arguments
 {
-        fprintf(stderr,
-                "\n"
-                "png2cpcsprite by Stéphane Gourichon (cpcitor).\n"
-                "\n"
-                "Convert a PNG image into a binary representation suitable for "
-                "display by an Amstrad CPC hardware or emulator.\n"
-                "\n"
-                "Currently the only output format supported is a generated "
-                "assembly file, with symbols."
-                "\n"
-                "usage: png2cpcsprite input-file.png output-file.generated.s "
-                "symbol_name mode deltay\n"
-                "\n"
-                "* input-file.png (mandatory) must be an image in PNG format "
-                "with a palette (colormap)\n"
-                "* output-file.generated.s (mandatory) name of output file\n"
-                "* symbol_name (optional) assembly-level symbol for the "
-                "generated file\n"
-                "* mode (optional) is cpc mode 0 1 or 2, or empty string for "
-                "guessing. \n"
-                "  If empty string or not supplied, will be guessed from "
-                "palette count.\n"
-                "* deltay (optional). If 1 or not supplied, sprite data will "
-                "be written top down.\n"
-                "  If -1, sprite data will be written bottom up."
-                "\n"
-                "Notice: \n"
-                "* The actual palette is ignored by this program.\n"
-                "* The generated ASM includes symbols for dimensions, byte "
-                "width, pixel width, height, size, and mode.\n"
-                "\n");
+        char *input_file;
+        char *output_file;
+        bool crtc_mode_explicitly_set;
+        u_int8_t crtc_mode;
+        bool bottom_to_top;
+        char *name_stem;
+        char *symbol_format_string;
+        char *module_format_string;
+        int verbose;
+};
+
+/* Parse a single option. */
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+        /* Get the input argument from argp_parse, which we
+           know is a pointer to our arguments structure. */
+        struct arguments *arguments = state->input;
+
+        char *reason = NULL;
+
+        switch (key)
+        {
+        case ARGP_KEY_INIT:
+                return 0;
+        case ARGP_KEY_NO_ARGS:
+                return 0;
+        case ARGP_KEY_END:
+                return 0;
+        case ARGP_KEY_SUCCESS:
+                return 0;
+        case ARGP_KEY_FINI:
+                return 0;
+        case ARGP_KEY_ARG:
+                reason = "stray argument";
+                goto invalid;
+                break;
+
+        default:
+                break;
+        }
+
+        fprintf(stderr, "key=%08xu '%c'\t", key, key);
+
+        printf("- argument '%s'\t... ", arg);
+        switch (key)
+        {
+        case 1: /* symbol_format_string */
+                arguments->symbol_format_string = arg;
+                goto ok;
+                break;
+        case 2: /* module_format_string */
+                arguments->module_format_string = arg;
+                goto ok;
+                break;
+        case 'n': /* name_stem */
+                arguments->name_stem = arg;
+                goto ok;
+                break;
+        default:
+                break;
+        }
+
+        /* All arguments take non-empty parameters.  Reject if empty */
+        if (arg[0] == 0)
+        {
+                reason = "empty string";
+                goto invalid;
+        }
+
+        switch (key)
+        {
+        case 'i':
+                arguments->input_file = arg;
+                break;
+        case 'o':
+                arguments->output_file = arg;
+                break;
+        case 'd':
+                // Assert only one character.
+                if (arg[1] != 0)
+                {
+                        reason = "more than one character";
+                        goto invalid;
+                }
+
+                if (arg[0] == 't')
+                {
+                        arguments->bottom_to_top = false;
+                        break;
+                }
+                if (arg[0] == 'b')
+                {
+                        arguments->bottom_to_top = true;
+                        break;
+                }
+                reason = "neither t nor b";
+                goto invalid;
+                break;
+
+        case 'm':
+                // Assert only one character.
+                if (arg[1] != 0)
+                {
+                        reason = "more than one character";
+                        goto invalid;
+                }
+
+                u_int8_t c = arg[0];
+
+                switch (c)
+                {
+                case '0':
+                case '1':
+                case '2':
+                        arguments->crtc_mode_explicitly_set = true;
+                        arguments->crtc_mode = c - 48;
+                        break;
+                case '-':
+                        arguments->crtc_mode_explicitly_set = false;
+                        break;
+                default:
+                        reason = "neither 0,1,2,-";
+                        goto invalid;
+                        break;
+                }
+
+                break;
+        default:
+                return ARGP_ERR_UNKNOWN;
+        }
+ok:
+        printf("ok\n");
+        return 0;
+invalid:
+        argp_error(state, "Invalid argument (%s): '%s'", reason, arg);
+        // Never reached.
+        return ARGP_ERR_UNKNOWN;
 }
+
+/* Our argp parser. */
+static struct argp argp = {options, parse_opt, 0 /* args_doc */, doc, 0, 0, 0};
 
 png_bytep read_png(const char *const input_file_name, png_image *image,
                    size_t *buffer_size)
@@ -129,102 +303,36 @@ u_int8_t guess_crtc_mode_based_on_colormap_entry_count(int colormap_entries)
 
 int main(int argc, const char **argv)
 {
-        if ((argc < 3) || (argc > (maxargs + 1)))
-        {
-                fprintf(stderr,
-                        "png2cpcsprite: "
-                        "Error: between 2 and " STR(
-                                maxargs) " arguments "
-                                         "expected, got %u.",
-                        argc - 1);
-                show_usage();
-                exit(2);
-        }
+        struct arguments arguments;
+        arguments.symbol_format_string = symbol_format_string_default;
+        arguments.module_format_string = module_format_string_default;
 
-        const char *const input_file_name = argv[1];
+        /* Parse our arguments; every option seen by parse_opt will
+           be reflected in arguments. */
+        argp_parse(&argp, argc, (char **restrict)argv, 0, 0, &arguments);
+
         png_image image;
         size_t buffer_size;
 
-        png_bytep buffer = read_png(input_file_name, &image, &buffer_size);
+        png_bytep buffer = read_png(arguments.input_file, &image, &buffer_size);
 
         printf("Finished decoding PNG. Processing.\n");
 
-        int crtc_mode;
-
-        if ((argc >= 4) && (argv[3][0] != 0))
-        {
-                printf("CRTC mode apparently supplied on command line.\n");
-                const char *const s_crtc_mode = argv[3];
-
-                switch (s_crtc_mode[0])
-                {
-                case '0':
-                case '1':
-                case '2':
-                        crtc_mode = s_crtc_mode[0] - 48;
-                        break;
-
-                default:
-                        fprintf(stderr,
-                                "png2cpcsprite: not a valid CPC mode: '%s'.\n",
-                                s_crtc_mode);
-                        exit(1);
-                }
-
-                if (s_crtc_mode[1])
-                {
-                        fprintf(stderr,
-                                "png2cpcsprite: invalid mode, must be single "
-                                "character, not '%s'.\n",
-                                s_crtc_mode);
-                        exit(1);
-                }
-        }
-        else
+        if (!arguments.crtc_mode_explicitly_set)
         {
                 printf("CRTC mode not determined by command line, guessing "
                        "from colormap count (%u entries).\n",
                        image.colormap_entries);
-                crtc_mode = guess_crtc_mode_based_on_colormap_entry_count(
-                        image.colormap_entries);
+                arguments.crtc_mode =
+                        guess_crtc_mode_based_on_colormap_entry_count(
+                                image.colormap_entries);
         }
 
-        printf("CRTC mode selected: %u.\n", crtc_mode);
+        printf("CRTC mode selected: %u.\n", arguments.crtc_mode);
 
-        int deltay;
+        unsigned int width_bytes = image.width >> (arguments.crtc_mode + 1);
 
-        if ((argc >= 6) && (argv[5][0] != 0))
-        {
-                printf("Deltay apparently supplied on command line.\n");
-                if (sscanf(argv[5], "%d", &deltay) == 0)
-                {
-                        fprintf(stderr,
-                                "png2cpcsprite: aborting: failure to parse "
-                                "deltay from: '%s'.\n",
-                                argv[5]);
-                        exit(1);
-                }
-        }
-        else
-        {
-                printf("deltay not determined by command line, using 1\n");
-                deltay = 1;
-        }
-
-        printf("Deltay selected: %d.\n", deltay);
-
-        if (deltay * deltay != 1)
-        {
-                fprintf(stderr,
-                        "png2cpcsprite: "
-                        "Error: deltay is not 1 or -1 but %d.",
-                        deltay);
-                exit(1);
-        }
-
-        unsigned int width_bytes = image.width >> (crtc_mode + 1);
-
-        unsigned int width_pixels = width_bytes << (crtc_mode + 1);
+        unsigned int width_pixels = width_bytes << (arguments.crtc_mode + 1);
 
         if (width_pixels != image.width)
         {
@@ -234,8 +342,8 @@ int main(int argc, const char **argv)
                         "image width %u "
                         "make around %u bytes "
                         "which will expand to %u pixels, not %u.",
-                        crtc_mode, image.width, width_bytes, width_pixels,
-                        image.width);
+                        arguments.crtc_mode, image.width, width_bytes,
+                        width_pixels, image.width);
                 exit(1);
         }
 
@@ -243,7 +351,8 @@ int main(int argc, const char **argv)
 
         printf("Will generate a sprite representation for CRTC mode %u, "
                "width %u pixels (%u bytes), height %u lines, total %u bytes.\n",
-               crtc_mode, image.width, width_bytes, image.height, sprite_bytes);
+               arguments.crtc_mode, image.width, width_bytes, image.height,
+               sprite_bytes);
 
         u_int8_t *sprite_buffer;
         {
@@ -260,12 +369,12 @@ int main(int argc, const char **argv)
                 }
         }
 
-        if (crtc_mode != 1)
+        if (arguments.crtc_mode != 1)
         {
                 fprintf(stderr,
                         "png2cpcsprite: error: only mode 1 supported at this "
                         "time, not %d.",
-                        crtc_mode);
+                        arguments.crtc_mode);
                 // Yes, we don't cleanup.  Quick and dirty!
                 exit(1);
         }
@@ -312,35 +421,26 @@ int main(int argc, const char **argv)
                 }
         }
 
-        const char *const output_file_name = argv[2];
-
         printf("Generated %u bytes "
                "of sprite data, will write them to output file '%s'.\n",
-               sprite_bytes, output_file_name);
+               sprite_bytes, arguments.output_file);
 
-        const char *symbol_name;
-        if (argc >= 5)
+        if (!arguments.name_stem)
         {
-                symbol_name = argv[4];
-                printf("Symbol name supplied on command line: '%s'.\n",
-                       symbol_name);
-        }
-        else
-        {
-                printf("No symbol name supplied on command line.\n");
+                printf("No name stem supplied on command line.\n");
 
                 const char *last_part_of_input_file_name =
-                        strrchr(input_file_name, '/');
+                        strrchr(arguments.input_file, '/');
                 if (last_part_of_input_file_name == NULL)
                 {
-                        last_part_of_input_file_name = input_file_name;
+                        last_part_of_input_file_name = arguments.input_file;
                 }
                 else
                 {
                         last_part_of_input_file_name++;
                 }
 
-                char *pre_symbol_name = strdup(last_part_of_input_file_name);
+                char *auto_name_stem = strdup(last_part_of_input_file_name);
 
                 /*
                   From
@@ -355,15 +455,15 @@ int main(int argc, const char **argv)
 
                  */
 
-                char *symbol_name_end =
-                        pre_symbol_name + strlen(pre_symbol_name);
+                char *auto_name_stem_end =
+                        auto_name_stem + strlen(auto_name_stem);
 
                 const char *valid_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghij"
                                           "klmnopqrstuvwxyz0123456789$._";
 
                 {
-                        char *p = pre_symbol_name;
-                        while (p != symbol_name_end)
+                        char *p = auto_name_stem;
+                        while (p != auto_name_stem_end)
                         {
                                 size_t advance = strspn(p, valid_chars);
                                 p += advance;
@@ -374,30 +474,34 @@ int main(int argc, const char **argv)
                         }
                 }
 
-                char symbol_name_auto[80];
-
-                snprintf(symbol_name_auto, 80, "%s%s", "sprite_",
-                         pre_symbol_name);
-                symbol_name_auto[79] = 0;
-
-                symbol_name = symbol_name_auto;
+                arguments.name_stem = auto_name_stem;
         }
 
-        printf("Will use symbol name '%s'\n", symbol_name);
+        printf("Will use symbol name '%s'\n", arguments.name_stem);
 
-        FILE *output_file = fopen(output_file_name, "w");
+        FILE *output_file = fopen(arguments.output_file, "w");
 
         if (output_file == NULL)
         {
                 fprintf(stderr,
                         "png2cpcsprite: error: could not open output file "
                         "'%s'.",
-                        output_file_name);
+                        arguments.output_file);
                 // Yes, we don't cleanup.  Quick and dirty!
                 exit(1);
         }
 
-        fprintf(output_file, ".module %s_module\n\n", symbol_name);
+#define MAX_STRINGS_SIZE 255
+
+        char symbol_name[MAX_STRINGS_SIZE];
+        snprintf(symbol_name, MAX_STRINGS_SIZE, arguments.symbol_format_string,
+                 arguments.name_stem);
+
+        char module_name[MAX_STRINGS_SIZE];
+        snprintf(module_name, MAX_STRINGS_SIZE, arguments.module_format_string,
+                 arguments.name_stem);
+
+        fprintf(output_file, ".module %s\n\n", module_name);
 
         fprintf(output_file, "%s_bytes == 0x%04x\n", symbol_name, sprite_bytes);
         fprintf(output_file, "%s_height == %d\n", symbol_name, image.height);
@@ -412,8 +516,9 @@ int main(int argc, const char **argv)
                 u_int8_t *b = sprite_buffer;
                 for (size_t yplain = 0; yplain < image.height; yplain++)
                 {
-                        u_int8_t y = (deltay > 0) ? yplain
-                                                  : image.height - 1 - yplain;
+                        u_int8_t y = arguments.bottom_to_top
+                                             ? image.height - 1 - yplain
+                                             : yplain;
                         u_int8_t bytes_on_this_line = 0;
 
                         b = &(sprite_buffer[width_bytes * y]);
@@ -445,7 +550,7 @@ int main(int argc, const char **argv)
 
         fclose(output_file);
 
-        printf("Finished writing file '%s'.\n", output_file_name);
+        printf("Finished writing file '%s'.\n", arguments.output_file);
 
         printf("Success. Exiting.\n");
 

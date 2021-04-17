207b3feecb8164d6572450eada7077517270a5d8
Stéphane Gourichon (cpcitor) <stephane_cpcitor@gourichon.org>
2021-04-17 18:04:22 +0200

    WIP stars 1 pixel per frame.

--- samples/2dstars_01_differential/2dstars.s
+++ samples/2dstars_01_differential/2dstars.s
@@ -2,7 +2,8 @@
 
         .z80
 
-        .globl _star_pos_byte_l
+        .globl _star_4pixelsperframe_pos_byte_l
+        .globl _star_1pixelsperframe_pos_byte_l
         .globl _setborder
 
 .include "cdtc/cpc_hardware_colors.s"
@@ -13,8 +14,6 @@ _show_stars::
         ;; ld bc,#0xbd40
         ;; out (c),c
 
-        ld a,#00
-
 next_frame:
         ld l,#hardware_color_r0_g0_b0_black
         call _setborder
@@ -22,11 +21,11 @@ next_frame:
         ld l,#hardware_color_r0_g0_b2_bright_blue
         call _setborder
 
-        ld hl, #(_star_pos_byte_l)
+        ld hl, #(_star_4pixelsperframe_pos_byte_l)
         ;; b = (star_count+1) mod 256
         ;; c = mask
         ld bc,#0x0088
-next_star:
+next_star4pix_:
         ;; read old address
         ld e,(hl)
         inc h
@@ -52,7 +51,69 @@ next_star:
         ;; next star
         inc l
 
-        djnz next_star
+        djnz next_star4pix_
+
+star1pix:
+        ld l,#hardware_color_r0_g1_b0_green
+        call _setborder
+
+        ;; star1pix encode both address and mask
+
+        ld hl, #(_star_1pixelsperframe_pos_byte_l)
+        ;; b = (star_count+1) mod 256
+        ;; c = mask
+        ld bc,#0x0088
+next_star1pix_:
+        ;; read old address
+        ld e,(hl)
+        inc h
+        ld d,(hl)
+
+        ;; erase old star (crude, whole byte)
+        xor a
+        ld (de),a
+
+        ;; prepare new address computation
+        ld b,a ; b=0	1 nop
+        ld c,a ; c=0	1 nop
+
+        ;; get mask
+        inc h
+        ld a,(hl)
+
+        ;; compute new mask
+        rlca   ;    	1 nop
+
+        ;; compute new address
+        rl c   ;    	2 nop
+        ex de,hl ;    	1 nop
+        sbc hl,bc;    	4 nop
+        set 6,h
+
+        ;; hl = new address
+        ;; de = table(mask)
+
+        ;; draw new star
+        ld (hl),a
+
+        ;; store new mask
+        ld (de),a
+        dec d
+
+        ex de,hl
+
+        ;; hl = table(high)
+        ;; de = new address
+
+        ;; store new address
+        ld (hl),d
+        dec h
+        ld (hl),e
+
+        ;; next star
+        inc l
+
+        djnz next_star1pix_
 
         jr next_frame
 
--- samples/2dstars_01_differential/main.c
+++ samples/2dstars_01_differential/main.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h> //random
 
-uint8_t __at( 0x3e00 ) star_pos_byte_l[256];
-uint8_t __at( 0x3f00 ) star_pos_byte_h[256];
+uint8_t __at( 0x1000 ) star_4pixelsperframe_pos_byte_l[256];
+uint8_t __at( 0x1100 ) star_4pixelsperframe_pos_byte_h[256];
+uint8_t __at( 0x1200 ) star_1pixelsperframe_pos_byte_l[256];
+uint8_t __at( 0x1300 ) star_1pixelsperframe_pos_byte_h[256];
+uint8_t __at( 0x1400 ) star_1pixelsperframe_mask[256];
 
 void show_stars( void );
 
@@ -32,8 +35,17 @@ main( void )
     {
         uint16_t addr = rand();
 
-        star_pos_byte_l[i] = addr & 0xff;
-        star_pos_byte_h[i] = 0xc0 | ( ( addr >> 8 ) & 0x3f );
+        star_4pixelsperframe_pos_byte_l[i] = addr & 0xff;
+        star_4pixelsperframe_pos_byte_h[i] = 0xc0 | ( ( addr >> 8 ) & 0x3f );
+    }
+
+    for ( int i = 0; i < 256; i++ )
+    {
+        uint16_t addr = rand();
+
+        star_1pixelsperframe_pos_byte_l[i] = addr & 0xff;
+        star_1pixelsperframe_pos_byte_h[i] = 0xc0 | ( ( addr >> 8 ) & 0x7f );
+        star_1pixelsperframe_mask[i] = (i<128)?0x80:0x08;
     }
 
     show_stars();

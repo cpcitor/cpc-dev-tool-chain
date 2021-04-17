bc981157f6f68658c2a70ec3b6cc6dea8c81a4a6
Stéphane Gourichon (cpcitor) <stephane_cpcitor@gourichon.org>
2021-04-17 17:26:31 +0200

    Custom palette.

--- samples/2dstars_00_bytes/main.c
+++ samples/2dstars_00_bytes/main.c
@@ -8,12 +8,36 @@ uint8_t __at( 0x3f00 ) star_pos_byte_h[256];
 
 void show_stars( void );
 
+const ink_vector16 palette = { {
+    /* border */
+    hardware_color_r0_g0_b0_black,
+    /* 16 palette entries */
+    hardware_color_r0_g0_b0_black,
+    hardware_color_r1_g0_b0_red,
+    hardware_color_r1_g1_b0_yellow,
+    hardware_color_r1_g1_b1_white,
+    hardware_color_r0_g1_b1_cyan,
+    hardware_color_r0_g0_b1_blue,
+    hardware_color_r2_g0_b0_bright_red,
+    hardware_color_r2_g1_b0_orange,
+    hardware_color_r2_g2_b0_bright_yellow,
+    hardware_color_r2_g2_b1_pastel_yellow,
+    hardware_color_r2_g2_b2_bright_white,
+    hardware_color_r1_g2_b2_pastel_cyan,
+    hardware_color_r1_g1_b2_pastel_blue,
+    hardware_color_r0_g2_b2_bright_cyan,
+    hardware_color_r0_g1_b2_sky_blue,
+    hardware_color_r0_g0_b2_bright_blue,
+} };
+
 void
 main( void )
 {
     fw_scr_set_mode( 0 );
 
     fw_kl_choke_off__ignore_return_value();
+    fw_mc_set_inks__16( &palette );
+
     cfwi_txt_str0_output( "Staaars!  Reset CPC or exit emulator to stop." );
 
     for ( int i = 0; i < 256; i++ )

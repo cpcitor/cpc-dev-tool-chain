aa0476ffad78c3abcebe44241de6f12049e657b4
Stéphane Gourichon (cpcitor) <stephane_cpcitor@gourichon.org>
2021-04-17 17:19:43 +0200

    make clang-format

--- samples/2dstars_00_bytes/main.c
+++ samples/2dstars_00_bytes/main.c
@@ -3,25 +3,25 @@
 #include <stdio.h>
 #include <stdlib.h> //random
 
-uint8_t __at (0x3e00) star_pos_byte_l[256];
-uint8_t __at (0x3f00) star_pos_byte_h[256];
+uint8_t __at( 0x3e00 ) star_pos_byte_l[256];
+uint8_t __at( 0x3f00 ) star_pos_byte_h[256];
 
-void show_stars(void);
+void show_stars( void );
 
 void
-main(void)
+main( void )
 {
-    fw_scr_set_mode(0);
-    cfwi_txt_str0_output("Staaars!  Reset CPC or exit emulator to stop.");
+    fw_scr_set_mode( 0 );
 
     fw_kl_choke_off__ignore_return_value();
+    cfwi_txt_str0_output( "Staaars!  Reset CPC or exit emulator to stop." );
 
-    for (int i=0; i<256; i++)
+    for ( int i = 0; i < 256; i++ )
     {
         uint16_t addr = rand();
 
-        star_pos_byte_l[i]=addr&0xff;
-        star_pos_byte_h[i]=0xc0 | ((addr>>8)&0x3f);
+        star_pos_byte_l[i] = addr & 0xff;
+        star_pos_byte_h[i] = 0xc0 | ( ( addr >> 8 ) & 0x3f );
     }
 
     show_stars();

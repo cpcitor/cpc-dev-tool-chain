8803064284cc9a75d3af7c1aae29d87df2399b5b
Stéphane Gourichon (cpcitor) <stephane_cpcitor@gourichon.org>
2021-04-18 13:19:02 +0200

    Comment, including timing.

--- cpclib/cdtc/fctdm_mul_x8_ll8_y8__x_gte_y__xpy_lt_128__xmy_lt_128.s
+++ cpclib/cdtc/fctdm_mul_x8_ll8_y8__x_gte_y__xpy_lt_128__xmy_lt_128.s
@@ -13,8 +13,8 @@
         ;;
         ;; 0 < l < h <=128
         ;; computes
-        ;; h*l>>8
-        ;; and returns the result in a and l.
+        ;; h*l
+        ;; and returns the result in h l.
         ;;
 
         ;; Inspired from https://www.msxcomputermagazine.nl/mccw/92/Multiplication/en.html
@@ -61,42 +61,50 @@ _fctdm_mul_x8_ll8_y8__x_gte_y__xpy_lt_128__xmy_lt_128_fill_table::
         jr nz,1$
         ret
 
+        ;; Input: h and l number to multiply.
+        ;; It is assumed that 0 <     h+l  < 128.
+        ;; It is assumed that 0 < abs(h-l) < 128.
 
+        ;; Total time in typical case: 34 NOPs
 _fctdm_mul_x8_ll8_y8__x_gte_y__xpy_lt_128__xmy_lt_128::
         ;; assumes h > l > 0
-        ld a,h
-        sub l
-        jp nc, numbers_in_order
-        neg
+        ld a,h                  ;	1
+        sub l                   ;	1
+        jp nc, numbers_in_order ;	if taken, 3, else 2
+        neg                     ;	1
+        ;; jp and neg combined make a total of 3 in both cases
 numbers_in_order:
-	ld d,#(>_fctdm_mul_x8_ll8_y8__x_gte_y__xpy_lt_128__xmy_lt_128_table)
-        ld e,a                  ; e=h0-l0
-        ex de,hl
-        ld c,(hl)
-        inc h                   ; switch to MSB table
-        ld b,(hl)
-        ; bc=(h0-l0)^2
+	ld d,#(>_fctdm_mul_x8_ll8_y8__x_gte_y__xpy_lt_128__xmy_lt_128_table) ;	1
+        ld e,a                  ; e=h0-l0                                    ;	1
+        ex de,hl                ;	1
+        ld c,(hl)               ;	2
+        inc h                   ; switch to MSB table           ;	     1
+        ld b,(hl)               ;       2
+        ; bc=(h0-l0)^2 >> 2
 
-        ex de,hl
-        ld a,h
-        add l
-        jp c, sum_of_operands_too_big
+        ex de,hl                ;       1
+        ld a,h                  ;       1
+        add l                   ;       1
+        jp c, sum_of_operands_too_big ; 3 (if not taken)
 
-        ld e,a
+        ld e,a                  ;       1
 
-        ex de,hl
-        ld a,(hl)               ; read MSB
-        dec h                   ; switch to LSB table
-        ld l,(hl)               ; read LSB
-        ld h,a
+        ex de,hl                ;       1
+        ld a,(hl)               ; read MSB      ;       2
+        dec h                   ; switch to LSB table   ;       1
+        ld l,(hl)               ; read LSB      ;       2
+        ld h,a                  ;       1
 
-        ; hl=(h0+l0)^2
+        ; hl=(h0+l0)^2 >> 2
 
-        ;; let's bet carry = 0. Why ? Because table is not over memory
-	;; boundary so inc h set carry to zero.
+        ;; carry = 0 here because of the "jp c" above. If carry was
+	;; set we would not have taken this path, and instructions
+	;; in-between (ex, ld, dec) do not change carry flag.
 
-        sbc hl,bc
-        ret
+        sbc hl,bc               ;       4
+
+        ;; hl = ( (h0+l0)^2 - (h0-l0)^2 ) >> 2 = h0*l0
+        ret                     ;       3
 sum_of_operands_too_big:
         ld hl,#8
         ret
